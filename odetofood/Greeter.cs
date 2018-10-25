b1380c7838bd74fd5dbf36c6790d07fb9b7412d4
Obinna Odirionye <odirionye@gmail.com>
2018-10-25 02:58:52 +0100

    feat(aspnetcoreMvc): added the dotnetcore MVC package
    this is to help me build controllers in .net standard

---
+++ odetofood/Greeter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace odetofood
+{
+    public interface IGreeter
+    {
+        string GetGreeting(); 
+    }
+    public class Greeter: IGreeter
+    {
+        public string GetGreeting()
+        {
+            return "Hello from the Greeter!";
+        }
+    }
+}
--- odetofood/Startup.cs
+++ odetofood/Startup.cs
@@ -30,19 +30,21 @@ namespace odetofood
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IGreeter, Greeter>(); 
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IGreeter greeter)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseFileServer();
             app.Run(async (context) =>
             {
-                var message = Configuration["Greeting"];
+               // throw new Exception("Something went wrong!");
+                var message = greeter.GetGreeting();
                 await context.Response.WriteAsync(message);
             });
         }
